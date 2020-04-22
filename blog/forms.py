2c0eee204b16a2d3124592b3fbf0eb98984324c1
Leonardo <leonardo.14117125@student.itera.ac.id>
2020-04-22 17:43:25 +0700

    fixing little bugs

--- blog/forms.py
+++ blog/forms.py
@@ -6,7 +6,17 @@ class PostForm(forms.ModelForm):
     
     class Meta:
         model=Post
-        fields=('title','text',)
+        fields=('title','text','category',)
+
+
+    def clean_title(self):
+        t=self.cleaned_data['title']
+        
+        if len(t) < 5:
+            raise forms.ValidationError("panjang title Kurang dari 5 ")
+
+        return t   
+            
 
 class CommentForm(forms.ModelForm):
     
--- blog/models.py
+++ blog/models.py
@@ -10,26 +10,26 @@ class Category(models.Model):
     def __str__(self):
         return self.title
 
-    class Post(models.Model):
-        category= models.ManyToManyField(Category,related_name='posts')
-        author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-        title = models.CharField(max_length=200)
-        text = models.TextField()
-        created_date = models.DateTimeField(default=timezone.now)
-        published_date = models.DateTimeField(blank=True, null=True)
+class Post(models.Model):
+    category= models.ManyToManyField(Category,related_name='posts')
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
 
-        def publish(self):
-            self.published_date = timezone.now()
-            self.save()
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
 
-        def __str__(self):
-            return self.title
+    def __str__(self):
+        return self.title
 
-    class Comment(models.Model):
-        post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
-        user = models.CharField(max_length=200)
-        body = models.TextField()
-        created_date = models.DateTimeField(default=timezone.now)
+class Comment(models.Model):
+    post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
+    user = models.CharField(max_length=200)
+    body = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
 
-        def __str__(self):
-            return self.user
+    def __str__(self):
+        return self.user
